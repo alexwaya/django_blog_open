eaa1f12968797d163bf342ba0c64870b87be41ee
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-24 04:11:44 +0300

    pack it up

--- dblog/settings.py
+++ dblog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'dblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
